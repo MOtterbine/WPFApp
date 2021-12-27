beb97321adefcc92e40d89b44f154aafdc7aa0fb
Mike Otterbine <40795733+MOtterbine@users.noreply.github.com>
2021-12-27 11:38:57 -0500

    Initial Commit

---
+++ WpfApp/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Input;
+
+namespace WpfApp
+{
+    public class ViewModel : ViewModelBase
+    {
+        public string Title 
+        {
+            get => title;
+            private set
+            {
+                base.SetProperty(ref title, value);
+            } 
+        }
+        private string title = "Our Class Know Nothing!";
+
+
+        public void SetTitle(string title)
+        {
+            Title = title;
+        }
+
+        public void SetName(string title)
+        {
+            Title = title;
+        }
+
+
+
+        #region RunCommand
+
+        public ICommand SetTitleCommand => new RelayCommand(param => SetTitle("crap"));
+        public ICommand SetNameCommand => new RelayCommand(param => SetName("Name"));
+
+        #endregion RunCommand
+
+
+
+
+    }
+}
